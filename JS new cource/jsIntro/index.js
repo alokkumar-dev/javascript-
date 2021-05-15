8476e634da7c397b7f257fc7e477400c32ab1bf9
alokkumar-dev <alokshakya2601@gmail.com>
2021-05-15 13:32:50 +0530

    modifi file

--- JS new cource/jsIntro/index.js
+++ JS new cource/jsIntro/index.js
@@ -1,4 +1,9 @@
 var element;
+
 document.getElementById("header").innerHTML="<h3>Alok Kumar</h3>";
-element = document.getElementById("header").innerHTML;
+
+element = document.getElementById("header").setAttribute("style","border : 6px dotted yellow");
+
+element = document.getElementById("header").getAttribute("class");
+
 console.log(element);
