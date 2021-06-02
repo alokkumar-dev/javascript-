e96bde6c61ee182852b3c685f8fcec77a99afb03
alokkumar-dev <alokshakya2601@gmail.com>
2021-06-02 12:42:44 +0530

    update file

--- JS new cource/jsIntro/index.js
+++ JS new cource/jsIntro/index.js
@@ -1,42 +1,55 @@
-// var element;
+var element;
+
+
+element=document.getElementById("header").setAttribute("class","rrr");
+
+element=document.getElementById("header").getAttribute("class");
+console.log(element);
 
 
 
 
 
-document.getElementById("header").addEventListener("click", abc);
 
-function abc(){
-    // document.getElementById("header").classList.add("xyz", "efg");
-    // document.getElementById("header").classList.toggle(";
-}
-
-var a=document.getElementById("header").classList.contains("efg");
-console.log(a);
-
-// Add eventListener Method
-/*
-document.getElementById("header").addEventListener("mouseleave" , abc);
-
-document.getElementById("header").addEventListener("click" , xyz);
-
-function abc(){
-    document.getElementById("header").style.backgroundColor ="pink";
-}
+// // var element;
 
 
-function xyz(){
-    document.getElementById("header").removeEventListener('mouseleave' , abc);
-}
 
 
-*/
 
+// document.getElementById("header").addEventListener("click", abc);
 
-// document.getElementById("header").onmouseenter =abc;
 // function abc(){
-//     document.getElementById("header").style.backgroundColor ="green";
+//     // document.getElementById("header").classList.add("xyz", "efg");
+//     // document.getElementById("header").classList.toggle(";
 // }
 
-// console.log(element);
+// var a=document.getElementById("header").classList.contains("efg");
+// console.log(a);
+
+// // Add eventListener Method
+// /*
+// document.getElementById("header").addEventListener("mouseleave" , abc);
+
+// document.getElementById("header").addEventListener("click" , xyz);
+
+// function abc(){
+//     document.getElementById("header").style.backgroundColor ="pink";
+// }
+
+
+// function xyz(){
+//     document.getElementById("header").removeEventListener('mouseleave' , abc);
+// }
+
+
+// */
+
+
+// // document.getElementById("header").onmouseenter =abc;
+// // function abc(){
+// //     document.getElementById("header").style.backgroundColor ="green";
+// // }
+
+// // console.log(element);
 
